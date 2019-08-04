5d3d08545bc541fc86c1b2f92815e12bce5cfe5e
Unknown <icktime@163.com>
2019-08-04 12:51:28 +0800

    完善所有功能
    缺陷是为调用数据库存储

--- todo/urls.py
+++ todo/urls.py
@@ -9,6 +9,8 @@ from . import views
 urlpatterns = [
     path('', views.home,name='待办事项主页'),
     path('about/', views.about,name='待办事项关于'),
-    path('edit/', views.edit,name='编辑'),
+    path('edit/<forloop_counter>', views.edit,name='编辑'),
+    path('del/<forloop_counter>', views.t_del,name='删除'),
+    path('sus/<forloop_counter>', views.sus,name='划掉'),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 lst = [
     {'待办事项':'遛狗','已完成':False},
-    {'待办事项':'遛狗','已完成':True}
+    {'待办事项':'打豆豆','已完成':True}
 ]
 for i,j in lst:
     print('%s,%s'%(i,j))
@@ -12,11 +12,12 @@ def home(request):
     content = {'content': lst}
     if request.method == 'POST':
         if request.POST['待办事项'] == '':
-            return render(request, 'home.html', {'warning':'请输入'},content)
+            content_warning = {'content':lst,'warning':'请输入内容!'}
+            return render(request, 'home.html',content_warning)
         else:
             lst.append({'待办事项':request.POST['待办事项'],'已完成':False})
-
-            return render(request,'home.html',content)
+            content_success = {'content':lst,'success':'添加成功!'}
+            return render(request,'home.html',content_success)
     else:
 
         return render(request, 'home.html', content)
@@ -24,5 +25,27 @@ def home(request):
 def about(request):
     return render(request,'todolistabout.html')
 
-def edit(request):
-    return render(request,'todolisteditt.html')
+def edit(request,forloop_counter):
+    content = {'待修改事项': lst[int(forloop_counter) - 1]['待办事项']}
+    if request.method == 'POST':
+        if request.POST['已修改事项'] == '':
+            return render(request, 'todolisteditt.html', {'warning':'请输入内容!'})
+        else:
+            lst[int(forloop_counter) - 1]['待办事项'] = request.POST['已修改事项']
+            return redirect('待办事项主页')
+    else:
+
+        return render(request,'todolisteditt.html',content)
+
+def t_del(request,forloop_counter):
+    lst.pop(int(forloop_counter) - 1)
+
+    return redirect('待办事项主页')
+
+def sus(request,forloop_counter):
+    if lst[int(forloop_counter) - 1]['已完成'] == False:
+        lst[int(forloop_counter) - 1]['已完成'] = True
+        return redirect('待办事项主页')
+    else:
+        lst[int(forloop_counter) - 1]['已完成'] = False
+        return redirect('待办事项主页')
